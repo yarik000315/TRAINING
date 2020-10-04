33752d991df64c9179620b9c6d93882cac347ba5
Yaroslav Kutsyi <yarik000315@gmail.com>
2020-10-04 19:34:06 +0300

    new_file_js_training

---
+++ user-windows.js
@@ -0,0 +1,10 @@
+// PROMPT
+// const user_windows_prompt = prompt("password");
+// console.log(user_windows_prompt);
+
+// ALERT
+// alert("Привіт");
+
+// CONFIRM
+// const isContinue = confirm("Бажаєте продовжити");
+// console.log(isContinue);
