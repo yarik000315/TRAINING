96a1eb2a9d3661721bf3f8a898247a1c3fb54cf6
yarik000315 <yarik000315@gmail.com>
2021-02-10 16:45:47 +0200

    add_module1

--- ПРАЦЮЮ НАД JS/module_1/task_5.js
+++ ПРАЦЮЮ НАД JS/module_1/task_5.js
@@ -12,3 +12,26 @@
 // Австралия - 170 кредитов
 // Индия - 80 кредитов
 // Ямайка - 120 кредитов
+
+const countryOfDeliveryChina = "КИТАЙ";
+const countryOfDeliveryChili = "ЧИЛИ";
+const countryOfDeliveryAustralia = "АВСТРАЛИЯ";
+const countryOfDeliveryIndia = "ИНДИЯ";
+const countryOfDeliveryJamaica = "ЯМАЙКА";
+
+const costOfDeliveryChina = 100;
+const costOfDeliveryChili = 250;
+const costOfDeliveryAustralia = 170;
+const costOfDeliveryIndia = 80;
+const costOfDeliveryJamaica = 120;
+
+let costDelivery;
+const messageUser = prompt("Укажите страну доставки");
+
+let messegeUserToUpperCase = messageUser.toUpperCase();
+
+switch (messegeUserToUpperCase) {
+  case countryOfDeliveryChina:
+    costDelivery = costOfDeliveryChina;
+    break;
+}
