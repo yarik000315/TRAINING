940c04ae864336a388f78f063e6fecf0258678b1
yarik000315 <yarik000315@gmail.com>
2021-03-01 09:46:47 +0200

    add_homeWorkJsModule3

--- ПРАЦЮЮ НАД JS/module_3/task_1.js
+++ ПРАЦЮЮ НАД JS/module_3/task_1.js
@@ -11,3 +11,32 @@
 //   hobby: 'html',
 //   premium: true,
 // };
+
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+
+console.log(user);
+
+for (const us in user) {
+  console.log("key -", us);
+}
+for (const us in user) {
+  console.log("value -", user[us]);
+}
+
+const entries = Object.entries(user);
+console.log(entries);
+
+for (const entry of entries) {
+  const key = entry[0];
+  const value = entry[1];
+  console.log(key, ":", value);
+}
--- ПРАЦЮЮ НАД JS/module_3/task_2.js
+++ ПРАЦЮЮ НАД JS/module_3/task_2.js
@@ -13,3 +13,12 @@
 // console.log(countProps({ name: 'Mango', age: 2 })); // 2
 
 // console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
+const countProps = function (obj) {
+  const keys = Object.keys(obj);
+  return keys.length;
+};
+
+console.log(countProps({})); // 0
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
