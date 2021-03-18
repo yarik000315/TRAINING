366e1044b5f334f3fddd4fbda47ee42f6da71e44
yarik000315 <yarik000315@gmail.com>
2021-03-18 12:45:33 +0200

    add_folderWithCodewars

---
+++ Codewars/codewars.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// NEW TASK
+// -------------------------
+// Highest and Lowest
+// -------------------------
+// function highAndLow(numbers) {
+//   const arrOfNumbers = numbers.split(" ");
+//   const high = Math.max.apply(null, arrOfNumbers);
+//   const low = Math.min.apply(null, arrOfNumbers);
+//   return `${high} ${low}`;
+// }
+// -------------------------
+// Test.assertEquals(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214");
+// -------------------------
+
+// NEW TASK
--- Пусті файли/java_script.js
+++ Пусті файли/java_script.js
@@ -1,9 +1,1 @@
 "use strict";
-
-const age = 27;
-const name = "Yarik";
-
-const message = `мене звати ${name}. Мені ${age} років!! !! `;
-
-console.log(message);
-let dog = 15;
