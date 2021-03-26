d4c0c550fa0ceec86a4853a4e28f5e85cd857040
yarik000315 <yarik000315@gmail.com>
2021-03-26 14:44:01 +0200

    addAnswer

--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -1,1 +1,33 @@
 "use strict";
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between including them too and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// function getSum(a, b) {
+//   if (a === b) {
+//     return a || b;
+//   } else {
+//     return a + b;
+//   }
+// }
+// console.log(getSum(1, 1));
+
+// function getSum(a, b) {
+//   return a === b ? a || b : a + b;
+// }
+// console.log(getSum(1, 1));
+
+// 1 and 9
+// n = a +1 поки не буде дорівнювати b
+function getSum(a, b) {
+  const cat = [];
+  arr.length = b;
+
+  for (let i = a; i < arr.length; i++) {
+    console.log(i);
+
+   
+}
+
+getSum(-1, 9);
