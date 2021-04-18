e0d7027a26ebd9c805977d36ea75f549d57e62e8
yarik000315 <yarik000315@gmail.com>
2021-04-18 22:02:37 +0300

    addSomeText

--- Пусті файли/java_script.js
+++ Пусті файли/java_script.js
@@ -1,1 +1,11 @@
 "use strict";
+// const firstFun = function (message) {
+//   console.log(message);
+// };
+
+// const secondFun = function (callback) {
+//   const string = "Some text about func";
+//   callback(string);
+// };
+
+// secondFun(firstFun);
